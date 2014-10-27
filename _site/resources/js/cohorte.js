1e1b1359edb3dd61b79fd0c6a92d9012b4f61137
debbabi <dbassem@gmail.com>
2014-10-27 16:04:04 +0100

    update

---
+++ _site/resources/js/cohorte.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+  // background image on frontpage
+  $(".splash").backstretch("/resources/images/alpes.jpg");
+
+  // get current year and put it in span
+  var currYear = new Date().getFullYear()
+  $(".current-year").text(currYear);
+
+  // same height hack for scala in a nutshell boxes
+  function makeAllBoxesSameHeight(boxes) {
+    maxHeight = Math.max.apply(
+      Math, boxes.map(function() {
+          return $(this).height();
+    }).get());
+    boxes.height(maxHeight);
+  }
+  var nutshell = $(".bullet-point").not(".span12");
+  makeAllBoxesSameHeight(nutshell);
+
+  // expanding code snippets (front page)
+  function expandSnippetAction(snippetID, container) {
+    var codeBox = container.find(".row");
+
+    function go() {
+      var snippet = $(snippetID).html();
+
+      // for positioning the arrow
+      var arrow = $(this).parent().siblings(".code-snippet-arrow");
+      var centerPoint = $(this).position().left + $(this).width()/2;
+      arrow.css("left", centerPoint);
+
+      var codeSnippetInContainer = codeBox.html();
+
+      if (container.is(":hidden")) {
+        arrow.show();
+        arrow.addClass("hover");
+        codeBox.html(snippet);
+        container.slideDown();
+      } else if (codeSnippetInContainer == snippet) {
+        container.slideUp(function() {
+          arrow.hide();
+        });
+      } else {
+        var hgt = $(snippetID).height();
+        arrow.addClass("hover");
+        codeBox.html(snippet);
+        codeBox.animate({height: hgt}, 400);
+      }
+    }
+    return go;
+  }
+
+  var row1 = $("#code-snippet-row1");
+  var row2 = $("#code-snippet-row2");
+
+  $("#key-point-1").click(expandSnippetAction("#hidden-key-point-1", row1));
+  $("#key-point-2").click(expandSnippetAction("#hidden-key-point-2", row1));
+  $("#key-point-3").click(expandSnippetAction("#hidden-key-point-3", row1));
+
+  $("#key-point-4").click(expandSnippetAction("#hidden-key-point-4", row2));
+  $("#key-point-5").click(expandSnippetAction("#hidden-key-point-5", row2));
+  $("#key-point-6").click(expandSnippetAction("#hidden-key-point-6", row2));
+
+
+});
+
+
